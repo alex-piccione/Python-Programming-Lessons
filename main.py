dd8df71bb2cd76805761e085846329d3d8b05941
Alex75 <alessandro.piccione.75@gmail.com>
2017-03-18 15:18:07 +0000

    add files

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 def main():
 
+
+
+    distances = []
+    cities = 100
+    distance_limit = 100
+    expected_result = None
+
+    # Act
+    result = main.get_max_distances(distances, cities, distance_limit)
+
+
+
+    print("lesson 1")
+
+    a = input()
+
     pass
 
 
