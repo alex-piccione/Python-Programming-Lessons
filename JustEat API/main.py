85c1a0db3336d99ebd9a4d21b1adcdc2679bb453
Alex75 <alessandro.piccione.75@gmail.com>
2017-12-26 22:19:40 +0000

    Stars !!!

--- JustEat API/main.py
+++ JustEat API/main.py
@@ -3,7 +3,6 @@ from JustEatApiCaller  import JustEatApiCaller
 postocde = "SE17"
 
 def start():
-
     caller = JustEatApiCaller()
     restaurants = caller.get_restaurants(postocde)
 
@@ -11,12 +10,19 @@ def start():
     print("Found {0} restaurants:\n".format(len(restaurants))) # print the number of found restaurants
 
     # print the headers
-    print("Name                                               | Rating | Menu ID")
-    print("-" * 70) # print "-" 70 times
+    print("Name                                               | Rating                         | Menu ID")
+    print("-" * 100) # print "-" 70 times
     for restaurant in restaurants:
         # str.ljust(size) justify the text on the left and fill the extra space (to reach the "size") with the "dot" character 
-        print("{0} | {1}    | {2}".format(restaurant.name.ljust(50, "."), restaurant.rating, restaurant.menu.id))
-
+        # rating is a value in the range 0-6 with 0.5 step, so we double it to ahve integer values and cast to int to print from 0 to 12 stars
+        # the extra logic to have a precise lenght is because seems like ljust does not work properly witth Uniocode symbols/characters
+        # then we nedd to "fill" the 10 spaces with another Unicode character that take the same space
+        stars = int(restaurant.rating * 2)
+        print("{0} | {1} | {2}".format(
+            restaurant.name.ljust(50, "."),
+            #( "*" * int(restaurant.rating*2) ).ljust(12),    # Python 2
+            "{0}{1}".format( "\u2B50" * stars, "\u2B55" * (12-stars)), # that is nice but 
+            restaurant.menu.id))
         
 
 start()
