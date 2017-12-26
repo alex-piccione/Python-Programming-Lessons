fa5a73c67fdc61c4958dae5f4a3a89d311dc35d6
Alex75 <alessandro.piccione.75@gmail.com>
2017-12-26 21:28:06 +0000

    Improve format of output

--- JustEat API/main.py
+++ JustEat API/main.py
@@ -7,12 +7,15 @@ def start():
     caller = JustEatApiCaller()
     restaurants = caller.get_restaurants(postocde)
 
-    print("Found {0} restaurants".format(len(restaurants))) # print the number of found restaurants
+    # \n add a "new line" character (it creates an empty line)
+    print("Found {0} restaurants:\n".format(len(restaurants))) # print the number of found restaurants
 
     # print the headers
-    print("Name             | Rating | Menu ID")
+    print("Name                                               | Rating | Menu ID")
+    print("-" * 70) # print "-" 70 times
     for restaurant in restaurants:
-        print("{0} | {1} | {2}".format(restaurant.name, restaurant.rating, restaurant.menu_id))
+        # str.ljust(size) justify the text on the left and fill the extra space (to reach the "size") with the "dot" character 
+        print("{0} | {1}    | {2}".format(restaurant.name.ljust(50, "."), restaurant.rating, restaurant.menu_id))
 
         
 
