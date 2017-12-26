3d5bd78bbe01ddbc77171f7ae90c0ce122c7b00c
Alex75 <alessandro.piccione.75@gmail.com>
2017-12-26 20:28:46 +0000

    Correct class name. add .json() method to obtain an usable object. Print names of restaurants.

--- JustEat API/JustEatApiCaller.py
+++ JustEat API/JustEatApiCaller.py
@@ -1,39 +1,32 @@
-'''Accept-Tenant: uk
-Accept-Language: en-GB
-Authorization: Basic VGVjaFRlc3RBUEk6dXNlcjI=
-Host: public.je-apis.com'''
-
-'''https://public.je-apis.com/restaurants?q=se19'''
-
 import requests
 
-class JustEastApiCaller:
+api_key = "Basic VGVjaFRlc3RBUEk6dXNlcjI="  # obtained from public recruitment test repository
 
-    def get_restaurants(self, postcode):  #page 307
+url_api_for_GET_restaurants = "https://public.je-apis.com/restaurants?q={0}"
 
-        print("{0}".format(postcode))  #string formatting
+class JustEatApiCaller:
 
+    def get_restaurants(self, postcode):
+
+        print("run get_restaurants for postcode {0}".format(postcode))
    
-
-        url = "https://public.je-apis.com/restaurants?q={0}".format(postcode)
+        url = url_api_for_GET_restaurants.format(postcode)
 
         headers = {
-"Accept-Tenant":"uk",
-"Accept-Language":"en-GB",
-"Authorization":"Basic VGVjaFRlc3RBUEk6dXNlcjI=",
-"Host":"public.je-apis.com"
-
+            "Accept-Tenant":"uk",
+            "Accept-Language":"en-GB",
+            "Authorization": api_key,
+            "Host":"public.je-apis.com"
         }
-        response = requests.get(url, headers=headers)    #requests is a library
 
-        print (response)
-        print (response.status_code)
-        #print (response.text) 
-        for restaurant in response.content.Restaurants:
-            print(restaurant.Name)
-        
-        print ("end") 
+        response = requests.get(url, headers=headers)
 
-    def _get_distance(point_a, point_b):
-        '''Returns the distance from a to b'''
-        pass
+        if (response.status_code == 200):
+            response_object = response.json()
+            for restaurant in response_object["Restaurants"]:
+                print(restaurant["Name"])                
+
+        else:
+            print("... something got wrong. Response status code: {0}. Reason: {1}".format(response.status_code, response.reason))
+       
+        print ("\n\nend") 
